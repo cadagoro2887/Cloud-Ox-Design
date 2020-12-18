6f3bead8a6bb1dd8ca5e5f5de763c3c8ff248bbd
Daniel Gonzalez <cadagoro@gmail.com>
2020-12-18 17:32:00 -0300

    Version 1.4: Agregado la seccion de Stats con sus respectivos estilos y una animacion de conteo en la clase React de la seccion Stats

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// COUNTER ANIMATION
+
+let nCount = selector => {
+    $(selector).each(function () {
+        $(this).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: "swing",
+            step: function (value) {
+                $(this).text(Math.ceil(value));
+
+            }
+        });
+    });
+};
+
+let a = 0;
+
+$(window).scroll(function () {
+    let oTop = $(".Count").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() >= oTop) {
+        a++;
+        nCount(".rect > h3")
+    }
+});
